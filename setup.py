eef6725f6e5b3daee114d614f9104cf00bec5ff7
acorbat <acorbat@df.uba.ar>
2020-03-30 17:31:36 -0300

    Added first version of setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='caspase_model',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/acorbat/caspase_model/tree/master/',
+    license='MIT',
+    author='Agustin Corbat',
+    author_email='acorbat@df.uba.ar',
+    description='Caspase model modified at Grecco Lab.',
+    install_requires=['pysb'],
+    dependency_links=['https://github.com/sorgerlab/earm.git']
+)
