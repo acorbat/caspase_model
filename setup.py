da4be60842b4012eeb4705328fc079e4a799ab37
acorbat <acorbat@df.uba.ar>
2020-04-02 10:29:08 -0300

    Fixing setup to include dependency link

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author='Agustin Corbat',
     author_email='acorbat@df.uba.ar',
     description='Caspase model modified at Grecco Lab.',
-    install_requires=['pysb', 'earm'],
-    dependency_links=['https://github.com/acorbat/earm/tarball/master#egg=package-1.0']
+    install_requires=['pysb'],
+    dependency_links=['https://github.com/acorbat/earm/tarball/master']
 )
